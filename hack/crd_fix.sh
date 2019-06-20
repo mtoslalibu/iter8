801d5b959f30d7e7aff147b42c7e7997c539a3f0
Lionel Villard <villard@us.ibm.com>
2019-06-20 14:09:00 -0400

    update controller-runtime dependency (#75)

--- hack/crd_fix.sh
+++ hack/crd_fix.sh
@@ -7,7 +7,7 @@ echo $SCRIPTDIR
 FILE_PATH=$SCRIPTDIR"/../config/crds/iter8_v1alpha1_experiment.yaml"
 suffix=".original"
 line=$(grep 'lastTransitionTime' -n config/crds/iter8_v1alpha1_experiment.yaml | sed 's/:.*//')
-line=$(( $line + 1 ))
+line=$(( $line + 5 ))
 
 sed -i$suffix  "${line}s/object/string/" $FILE_PATH
 rm $FILE_PATH$suffix
