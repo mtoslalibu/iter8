8ae5ac65ca4edd01b3e0453227b5bb03c8c3eb62
Zijun Wang <zijun.wang@ibm.com>
2019-12-19 10:24:21 -0500

    Revert "fix metric read"
    This reverts commit 0b91e00beffe024f97fbc7fb83f7172e6dc36621.

--- pkg/controller/experiment/common.go
+++ pkg/controller/experiment/common.go
@@ -161,7 +161,6 @@ type Metric struct {
 	IsCounter          bool   `yaml:"is_counter"`
 	AbsentValue        string `yaml:"absent_value"`
 	SampleSizeTemplate string `yaml:"sample_size_query_template"`
-	Type               string `yaml:"metric_type"`
 }
 
 func readMetrics(context context.Context, c client.Client, instance *iter8v1alpha1.Experiment) error {
@@ -206,7 +205,6 @@ func readMetrics(context context.Context, c client.Client, instance *iter8v1alph
 		m := iter8v1alpha1.ExperimentMetric{
 			IsCounter:   metric.IsCounter,
 			AbsentValue: metric.AbsentValue,
-			Type:        metric.Type,
 		}
 		qTpl, ok := templates[metric.Name]
 		if !ok {
