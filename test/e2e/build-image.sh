05c339bdeaca5003c43ac5beb2dc8ca88dc51c8f
Hai Huang <huang195@users.noreply.github.com>
2020-06-08 14:03:08 -0400

    optimize travis build (#175)

--- test/e2e/build-image.sh
+++ test/e2e/build-image.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-# Only build images for master and v[0-9]+.[0-9]+ release branches
-if [[ "$TRAVIS_BRANCH" == "master" ]] || [[ "$TRAVIS_BRANCH" =~ ^v[0-9]+\.[0-9]+$ ]] ; then
-  # Only build images when commits are done on these branches
-  if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
-    export IMG="iter8/iter8-controller:$TRAVIS_BRANCH";
-    echo "Building PR Docker image - $IMG";
-    make docker-build;
-    make docker-push;
-    #LATEST="iter8/iter8-controller:latest";
-    #echo "Tagging image as latest - $LATEST";
-    #docker tag $IMG $LATEST;
-    #export IMG=$LATEST;
-    #make docker-push;
-  fi
-fi
-
+echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
+export IMG="iter8/iter8-controller:$TRAVIS_BRANCH";
+echo "Building PR Docker image - $IMG";
+make docker-build;
+make docker-push;
+#LATEST="iter8/iter8-controller:latest";
+#echo "Tagging image as latest - $LATEST";
+#docker tag $IMG $LATEST;
+#export IMG=$LATEST;
+#make docker-push;
