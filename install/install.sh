485abc14b3d89883c311446b94e840b448af7df2
Hai Huang <huang195@gmail.com>
2020-06-17 08:41:07 -0400

    use istio config to detect telemetry version

--- install/install.sh
+++ install/install.sh
@@ -2,17 +2,14 @@
 
 #set -x
 
-verlte() {
-  [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
-}
-
-verlt() {
-  [ "$1" = "$2" ] && return 1 || verlte $1 $2
-}
+# Use default istio namespace unless ISTIO_NAMESPACE is defined
+: "${ISTIO_NAMESPACE:=istio-system}"
 
 install() {
 
-  ISTIO_VERSION=`kubectl -n istio-system get pods -o yaml | grep "image:" | grep proxy | head -n 1 | awk -F: '{print $3}'`
+  echo "Istio namespace: $ISTIO_NAMESPACE"
+  MIXER_DISABLED=`kubectl -n $ISTIO_NAMESPACE get cm istio -o json | jq .data.mesh | grep -o 'disableMixerHttpReports: [A-Za-z]\+' | cut -d ' ' -f2`
+  ISTIO_VERSION=`kubectl -n $ISTIO_NAMESPACE get pods -o yaml | grep "image:" | grep proxy | head -n 1 | awk -F: '{print $3}'`
 
   if [ -z "$ISTIO_VERSION" ]; then
     echo "Cannot detect Istio version, aborting..."
@@ -20,8 +17,9 @@ install() {
   fi
 
   echo "Istio version: $ISTIO_VERSION"
+  echo "Istio mixer disabled: $MIXER_DISABLED"
 
-  if verlt "$ISTIO_VERSION" "1.5"; then
+  if [ "$MIXER_DISABLED" = "false" ]; then
     echo "Using Istio telemetry v1"
     kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-controller/v0.2/install/iter8-controller.yaml
   else
