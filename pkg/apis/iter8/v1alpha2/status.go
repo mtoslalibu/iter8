6f6c0f6f9eb9f66d0e1cf1e88f5db755d3b582fc
Zijun Wang <zijun.wang@ibm.com>
2020-07-29 18:09:58 -0400

    Add iteration number to AssessmentUpdate events

--- pkg/apis/iter8/v1alpha2/status.go
+++ pkg/apis/iter8/v1alpha2/status.go
@@ -324,16 +324,17 @@ func (s *ExperimentStatus) IsWinnerFound() bool {
 
 // WinnerToString outputs winner assessment in human-readable format
 func (s *ExperimentStatus) WinnerToString() string {
+	progress := fmt.Sprintf("[Iteration %d]: ", *s.CurrentIteration)
 	if s.Assessment != nil && s.Assessment.Winner != nil {
 		if s.Assessment.Winner.WinnerFound {
-			return fmt.Sprintf("Current winner (%s) has winning probability of %f.", s.Assessment.Winner.Winner,
+			return progress + fmt.Sprintf("Current winner (%s) has winning probability of %f.", s.Assessment.Winner.Winner,
 				s.Assessment.Winner.Probability)
 		} else {
-			return fmt.Sprintf("Winner has not been found yet. Current best version (%s) has winning probability of %f.", s.Assessment.Winner.Winner,
+			return progress + fmt.Sprintf("Winner has not been found yet. Current best version (%s) has winning probability of %f.", s.Assessment.Winner.Winner,
 				s.Assessment.Winner.Probability)
 		}
 	} else {
-		return "Not available."
+		return progress + "Not available."
 	}
 }
 
